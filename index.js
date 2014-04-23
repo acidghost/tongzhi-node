4eb48a48d21d021493cf1f90d8897f3ba9075d92
Andrea Jemmett <andreajemmett@gmail.com>
2014-04-23 16:37:09 +0200

    Modularized the checker routines

--- index.js
+++ index.js
@@ -50,61 +50,8 @@ var server = http.createServer(function(req, res) {
 server.listen(process.env.port || config.port || 3737);
 
 events.on('hasFbToken', function() {
-  var checkFacebook = function() {
-    fbService.getUnseenLikes();
-    fbService.getUnreadMessages();
-  };
-  checkFacebook();
-  setInterval(checkFacebook, 1000 * 60);
 
-  if(config.say.enabled) {
-    var SpeakerService = require('./services/speaker');
-    spkService = new SpeakerService();
-  }
+  var CheckerService = require('./services/checker');
+  var checker = new CheckerService(fbService);
 
-  // This event is fired when unseen likes have been retrieved
-  events.on('fbUnseenLikes', function(data) {
-    log.debug('Debugging fbUnseenLikes: ', data);
-
-    if(data.length == 0)
-      return;
-
-    if(config.say.enabled) {
-      _.each(data, function(value) {
-        spkService.sayPhrase(value.title);
-      });
-    }
-  });
-
-  // This event is fired when unread messages have been detected
-  events.on('fbUnreadMessages', function(data) {
-    log.debug('Debugging fbUnreadMessages: ', data);
-
-    if(data.length == 0)
-      return;
-
-    var unreadMessages = 0;
-    _.map(data, function(value) {
-      unreadMessages += value.unread;
-    });
-
-    if(config.say.enabled) {
-      spkService.sayPhrase('You have ' + unreadMessages + ' unread message' + ((unreadMessages>1) ? ('s.') : ('.')));
-      _.each(data, function(thread, threadNum) {
-        // This removes the last 'to' user, which usually is the recipient
-        thread.to.pop();
-        var senders = '';
-        _.map(thread.to, function(sender) {
-          senders += sender.name;
-          if(thread.to.indexOf(sender) != thread.to.length-1) {
-            senders += ', ';
-          }
-        });
-        spkService.sayPhrase('Sender' + ((thread.to>1) ? ('s') : ('')) + ' of thread ' + (threadNum+1) + ': ' + senders);
-        if(config.say.readMessages && thread.lastMessage) {
-          spkService.sayPhrase(thread.lastMessage[2].from.name + ' writes: ' + thread.lastMessage[2].message);
-        }
-      });
-    }
-  });
 });
