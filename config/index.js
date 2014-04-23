cbb08a23f431ef27455b58d54e4601d95584e7c8
Andrea Jemmett <andreajemmett@gmail.com>
2014-04-23 16:35:24 +0200

    Commented out the config file

--- config/index.js
+++ config/index.js
@@ -4,11 +4,16 @@
 
 module.exports = {
 
+  // Logger verbosity
   logger: 'info',
+
+  // Checker refresh time in seconds
+  checkerRefresh: 60 * 5,
 
   // Remember to change also the redirecting URLs!
   port: 3737,
 
+  // Facebook app configurations
   facebook: {
     client_id: 'YOUR_FB_CLIENT_ID',
     client_secret: 'YOUR_FB_CLIENT_SECRET',
@@ -16,12 +21,13 @@ module.exports = {
     redirect_uri: 'http://localhost:3737/auth/facebook'
   },
 
+  // Speaker module configurations
   say: {
-    enabled: true,
-    voice: 'voice_kal_diphone',
-    readMessages: true,
-    translate: true,
-    language: 'Italian'
+    enabled: true,                    // weather it's enabled or not
+    voice: 'voice_kal_diphone',       // which voice to use (for Linux uses Festival)
+    readMessages: true,               // reads out loud messages (turn off for privacy concerns)
+    translate: true,                  // activates the built in translator
+    language: 'Italian'               // into which language to translate
   }
 
 };
